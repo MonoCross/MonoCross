5f312b67181d4cc0babe420f239c0b37dd24aa33
Kevin McMahon <kevin@kevfoo.com>
2010-12-30 22:30:16 -0600

    changed elements to relative layouts and fixed some formatting.

---
+++ MonoDroid.Dialog/DialogView.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Android.Util;
+using Android.Widget;
+
+namespace MonoDroid.Dialog
+{
+    public class DialogView : LinearLayout
+    {
+        public DialogView(Context context, Element element) : base(context)
+        {
+            Initialize(element);
+        }
+
+        public DialogView(Context context, IAttributeSet attrs, Element element) :
+            base(context, attrs)
+        {
+            Initialize(element);
+        }
+
+        private void Initialize(Element element)
+        {
+            SetMinimumHeight(44);
+            AddView(element.GetView());
+        }
+    }
+}
