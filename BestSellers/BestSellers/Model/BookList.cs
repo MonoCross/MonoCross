6a7213f085c4c6630049bc1b48059db07a8cb603
Ben Horgen <ben@benhorgen.com>
2014-01-07 14:04:16 -0600

    Cleaned out some ancient code (previously commented out).

--- BestSellers/BestSellers/Model/BookList.cs
+++ BestSellers/BestSellers/Model/BookList.cs
@@ -3,23 +3,7 @@
 namespace BestSellers
 {
     public class BookList : List<Book>
-    // ObservableCollection<Book> 
     {
-        /*
-        public string Category
-        {
-            get { return _category; }
-            set
-            {
-                if (_category != value)
-                {
-                    _category = value;
-                    OnPropertyChanged(new PropertyChangedEventArgs("Category"));
-                }
-            }
-        }
-        string _category;
-        */
         public string Category { get; set; }
         public string CategoryDisplayName { get; set; }
     }
