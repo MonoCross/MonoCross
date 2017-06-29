b8ab0e59ebd451809d2a06101d99a15255156b09
Samuel Lippert <SLippert@zebra.com>
2017-06-29 14:26:20 -0500

    Add AssemblyInformationalVersion to utils

--- Utilities/Properties/AssemblyInfo.cs
+++ Utilities/Properties/AssemblyInfo.cs
@@ -1,7 +1,8 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 #if DROID
@@ -14,13 +15,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MonoCross.Utilities.Net")]
 #endif
 
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MonoCross Utilities")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MonoCross Project")]
 [assembly: AssemblyProduct("MonoCross Utilities")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -33,14 +35,15 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.42")]
 #if !NETCF
 [assembly: AssemblyFileVersion("4.0.0.42")]
 #endif
+[assembly: AssemblyInformationalVersion("Branch - master (Hash: Working Tree)")]
