5ad304062e588443c90a7c9f8579bc002526c9fb
Samuel Lippert <SLippert@zebra.com>
2018-04-16 12:54:08 -0500

    Preserve unicode copyright symbol

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -23,6 +23,7 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyCopyright("Copyright ©  " +
+"2018")]
 [assembly: AssemblyVersion("4.0.0.42")]
 [assembly: AssemblyInformationalVersion("4.0.0.42-master.WorkingTree")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
